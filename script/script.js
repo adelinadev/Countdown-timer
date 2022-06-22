7fe55cf8fe917cdda7cc7f51752b53e0bf392844
AdelinaWood <CarpeDiemAdel@gmail.com>
2022-06-22 13:10:18 +0200

    created basics markup of the countdown make a Timing funcion

---
+++ script/script.js
@@ -0,0 +1,73 @@
+let deadline = '2022-07-02';
+let id = document.querySelector('.id');
+
+
+const timer = (x, y) => {
+
+    const getTimeRemaining = (endtime) => {
+
+        const addZero = () => {
+            if (num <= 9){
+                return '0' + num;
+            } else {
+                return num;
+            }
+        };
+
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000 * 60 * 60)) % 24),
+        days = Math.floor((t/(1000 * 60 * 60)) % 24);
+
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        }; 
+    };
+
+    const setClock = (selector, endtime) => {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector("#days"),
+              hours = timer.querySelector("#hours"),
+              minutes = timer.querySelector("#minutes"),
+              seconds = timer.querySelector("#seconds"),
+              timeInterval = setInterval(updateClock, 1000);
+        
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0){
+                days.textContent = "00";
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+    
+                clearInterval(timeInterval)
+            }
+        }
+
+    };
+
+    setClock( id, deadline);
+};
+
+timer(id, deadline);
+
+// Функция принимает два аргумента. Первый говорит нам в какую область(в какой элемент)
+// мы этот таймер будем рендерить(id) а также нам понадобится дедлайн (deadline) до какого момента 
+// у нас будет идти таймер. Разбиваем наш код на отдельные функции.
+// Время в JS измеряется милисекундами.
+// Объект Date метод parse().Этот метод принимает в себя стороку с датой, записанной в определённом формате
+//  Date.parse(new Date() - количество времени сейчас
+
+
